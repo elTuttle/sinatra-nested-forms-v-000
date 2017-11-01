5b852591310201d117a96397c971ffd480a0d1a3
John Tuttle <thejwt@gmail.com>
2017-11-01 20:51:57 +0000

    automatically backed up by learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -5,7 +5,7 @@ class Pirate
   PIRATES = []
 
   def initialize
-    
+
   end
 
   def self.all
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,15 @@
 class Ship
+
+  attr_accessor :name, :type, :booty
+
+  PIRATES = []
+
+  def initialize
+
+  end
+
+  def self.all
+    PIRATES
+  end
+  
 end
